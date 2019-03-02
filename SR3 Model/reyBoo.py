b11c16e8f5c22b6123832679e5e187f38b397f7d
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2019-03-01 18:58:12 -0600

    finished

--- SR3 Model/reyBoo.py
+++ SR3 Model/reyBoo.py
@@ -5,7 +5,7 @@ def reyBoo():
 	glCreateWindow(800,800)
 	glViewPort(0,0,800,800)
 	#utilizamos la funcion lineas
-	load('./modelos/test2.obj', (500,200),(0.6,0.6))
+	load('./modelos/reyBooC2.obj', (500,200),(0.6,0.6))
 	glFinish()
 
 print(reyBoo())
