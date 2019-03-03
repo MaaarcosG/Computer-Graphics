a589f4ab87020fe7150fe7a72306879385b9da72
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2019-03-03 13:35:36 -0600

    Codigo entendible

--- SR4 Flat Shading/main.py
+++ SR4 Flat Shading/main.py
@@ -3,7 +3,7 @@ from bitmap import *
 def rellenando():
 	rellenando = Bitmap(800,800)
 	glViewPort(0,0,800,800)
-	rellenando.load('./modelos/Test2.obj', (500,200),(0.5,0.5))
+	rellenando.load('./modelos/test2.obj', (500,200),(0.5,0.5))
 	rellenando.display('out.bmp')
 
 print(rellenando())
