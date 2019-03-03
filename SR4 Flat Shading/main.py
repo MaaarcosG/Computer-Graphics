e2c089728a8af66f435395c7bdad9b59465ad737
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2019-03-03 12:41:30 -0600

    change

---
+++ SR4 Flat Shading/main.py
@@ -0,0 +1,9 @@
+from bitmap import *
+
+def rellenando():
+	rellenando = Bitmap(800,800)
+	glViewPort(0,0,800,800)
+	rellenando.load('./modelos/Test2.obj', (500,200),(0.5,0.5))
+	rellenando.display('out.bmp')
+
+print(rellenando())
