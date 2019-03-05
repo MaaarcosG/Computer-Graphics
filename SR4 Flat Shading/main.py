f757cf984ed29e1eb495e426a124ffea63f9960e
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2019-03-05 17:03:40 -0600

    Mejoras en el main

--- SR4 Flat Shading/main.py
+++ SR4 Flat Shading/main.py
@@ -7,6 +7,7 @@ from bitmap import *
 def rellenando():
 	renderizando = Bitmap(1000,1000)
 	glViewPort(0,0,800,800)
+	#renderizando.renderer(./modelos/test3.obj, scale=(0,0,0), translate=(0,0,0))
 	renderizando.renderer('./modelos/test3.obj',(100,100,100),(4,3,3))
 	renderizando.archivo('out.bmp')
 
