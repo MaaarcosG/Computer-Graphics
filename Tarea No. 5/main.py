9ecea05eee5ea97342c298b590d07050e48d0d50
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2019-03-08 09:23:20 -0600

    Abriendo archivo

--- Tarea No. 5/main.py
+++ Tarea No. 5/main.py
@@ -6,12 +6,12 @@ from obj import Mtl
 # Carne: 17909
 
 def reyBoo():
-	renderizando = Bitmap(1000,1000)
+	renderizando = Bitmap(600,600)
 	glViewPort(0,0,800,800)
-	kd = Mtl('./modelos/ReyBoo.mtl')
 	#renderizando.renderer(./modelos/test3.obj, scale=(0,0,0), translate=(0,0,0))
 	#renderizando.renderer('./modelos/ReyBoo.obj',(100,100,100),(3,3,3))
-	renderizando.load_mtl('./modelos/ReyBoo.obj',(100,100,100),(3,3,3), kd)
+	#renderizando.load_mtl('./modelos/ReyBoo.obj',(100,100,100),(3,3,3), kd)
+	renderizando.load_mtl('./modelos/ReyBoo.mtl')
 	renderizando.archivo('output.bmp')
 
 print("Renderizando los modelos obj")
