fae544535046f4c4899639e9482af630dcf050d2
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2019-03-07 20:16:46 -0600

    Agregando Modelos

--- Tarea No. 5/main.py
+++ Tarea No. 5/main.py
@@ -8,8 +8,8 @@ def reyBoo():
 	renderizando = Bitmap(1000,1000)
 	glViewPort(0,0,800,800)
 	#renderizando.renderer(./modelos/test3.obj, scale=(0,0,0), translate=(0,0,0))
-	renderizando.renderer('./modelos/reyBoo.obj',(200,200,200),(3,3,3))
-	renderizando.archivo('reyBoo_Renderizado.bmp')
+	renderizando.renderer('./modelos/test2.obj',(100,100,100),(3,3,3))
+	renderizando.archivo('output.bmp')
 
 print("Renderizando los modelos obj")
 
